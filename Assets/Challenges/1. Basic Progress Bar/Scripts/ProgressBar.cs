463de450e90b02d9783bcc00a072424f44551a74
Epoxius <62517458+Epoxius@users.noreply.github.com>
2022-02-15 23:16:14 +0300

    Initial Commit

---
+++ Assets/Challenges/1. Basic Progress Bar/Scripts/ProgressBar.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Challenges._1._Basic_Progress_Bar.Scripts
+{
+    /// <summary>
+    /// Edit this script for the ProgressBar challenge.
+    /// </summary>
+    
+    public class ProgressBar : MonoBehaviour, IProgressBar
+    {
+        public Slider progressBar;
+
+        public void Update()
+        {
+            ForceValue(0);
+        }
+
+        private enum ProgressSnapOptions
+        {
+            SnapToLowerValue,
+            SnapToHigherValue,
+            DontSnap
+        }
+        
+        /// <summary>
+        /// You can add more options
+        /// </summary>
+        private enum TextPosition
+        {
+            BarCenter,
+            Progress,
+            NoText
+        }
+        
+        /// <summary>
+        /// These settings below must function
+        /// </summary>
+        [Header("Options")]
+        [SerializeField]
+        private float baseSpeed;
+        [SerializeField]
+        private ProgressSnapOptions snapOptions;
+        [SerializeField]
+        private TextPosition textPosition;
+        
+        
+        
+        /// <summary>
+        /// Sets the progress bar to the given normalized value instantly.
+        /// </summary>
+        /// <param name="value">Must be in range [0,1]</param>
+        public void ForceValue(float value)
+        {
+            progressBar.value += baseSpeed * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// The progress bar will move to the given value
+        /// </summary>
+        /// <param name="value">Must be in range [0,1]</param>
+        /// <param name="speedOverride">Will override the base speed if one is given</param>
+        public void SetTargetValue(float value, float? speedOverride = null)
+        {
+            
+        }
+
+    }
+}
